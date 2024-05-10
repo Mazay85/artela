ec025c0438256e491d5b19a53b1ad5501b1de8ff
Mazay85 <55110519+Mazay85@users.noreply.github.com>
2024-05-10 21:49:03 +0300

    Update cpart1.sh

--- cpart1.sh
+++ cpart1.sh
@@ -32,7 +32,6 @@ case $opt in
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:36657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6061\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36660\"%" $HOME/.artelad/config/config.toml
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9190\"%; s%^address = \"localhost:9091\"%address = \"localhost:9191\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1327\"%" $HOME/.artelad/config/app.toml
 sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36657\"%" $HOME/.artelad/config/client.toml
-sed -i.bak -e "s/^external_address *=.*/external_address = \"external_address:36656\"/" $HOME/.artelad/config/config.toml
 sudo systemctl restart artelad
 
 echo -e ' 26656-->36656
