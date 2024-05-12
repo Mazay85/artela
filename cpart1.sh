f8e3544182ef06bc369785ebe6a50a0e938688b8
Mazay85 <55110519+Mazay85@users.noreply.github.com>
2024-05-13 01:47:24 +0300

    Update cpart1.sh

--- cpart1.sh
+++ cpart1.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-while true
-do
-
-# Menu
-
-PS3='Select an action: '
-options=(
-"Change 26656-->36656"
-"Change 26656-->46656"
-"Change 26656-->56656"
-"Exit")
-select opt in "${options[@]}"
-do
-case $opt in
-
-"Change 26656-->36656")
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:36658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:36657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:6061\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:36656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":36660\"%" $HOME/.artelad/config/config.toml
 sed -i.bak -e "s%^address = \"localhost:9090\"%address = \"localhost:9190\"%; s%^address = \"localhost:9091\"%address = \"localhost:9191\"%; s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:1327\"%" $HOME/.artelad/config/app.toml
 sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:36657\"%" $HOME/.artelad/config/client.toml
@@ -30,10 +13,3 @@ echo -e ' 9090 --> 9190 '
 echo -e ' 9091 --> 9191 '
 echo -e ' 1317 --> 1327 '
 echo -e '\e[0m'
-exit
-;;
-*) echo "invalid option $REPLY";;
-esac
-done
-done
-
